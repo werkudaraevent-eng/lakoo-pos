06241e4138c6daa5d2537e5837abcdd9c3494751
werkudaraevent-eng <werkudara.event@gmail.com>
2026-05-07 10:23:13 +0700

    fix: DataManagementPage full-width (remove maxWidth constraint)

--- src/pages/DataManagementPage.jsx
+++ src/pages/DataManagementPage.jsx
@@ -100,7 +100,7 @@ export function DataManagementPage() {
   }
 
   return (
-    <div style={{ maxWidth: 800, margin: "0 auto" }}>
+    <div>
       {/* Tab switcher */}
       <div
         style={{
